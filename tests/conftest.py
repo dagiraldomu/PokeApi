204e5b682085cc18eb042872953d04f9f11e65bb
DanielG <d.anigiraldomu@gmail.com>
2024-10-20 09:38:56 -0500

    test: Mock added to handle fetch berries and test endpoint working.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,20 @@ from httpx import AsyncClient, ASGITransport
 from unittest import mock
 
 
-mock.patch("fastapi_cache.decorator.cache", lambda *args, **kwargs: lambda f: f).start()
+# Mock function for get_berries_from_cache_or_fetch
+def mock_get_berries_from_cache_or_fetch():
+    return {
+        "berries_names": ["berry1", "berry2"],
+        "min_growth_time": "10 hours",
+        "median_growth_time": "12.5 hours",
+        "max_growth_time": "20 hours",
+        "variance_growth_time": "2.5 hours",
+        "mean_growth_time": "15.0 hours",
+        "frequency_growth_time": {"10": 2, "12": 3, "20": 1}
+    }
+
+
+mock.patch("app.services.berries.get_berries_from_cache_or_fetch", mock_get_berries_from_cache_or_fetch).start()
 
 @pytest.fixture(scope="module")
 async def client():
--- tests/test_async.py
+++ tests/test_async.py
@@ -4,7 +4,23 @@ from app.main import app
 
 
 @pytest.mark.asyncio
-async def test_read_item():
+async def test_fetch_berry_stats():
+
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/allBerryStats")
+
     assert response.status_code == 200
+    # Check if the 'Content-Type' header is 'application/json'
+    assert response.headers["content-type"] == "application/json"
+    # Validate the response data matches the mock
+    expected_data = {
+        "berries_names": ["berry1", "berry2"],
+        "min_growth_time": "10 hours",
+        "median_growth_time": "12.5 hours",
+        "max_growth_time": "20 hours",
+        "variance_growth_time": "2.5 hours",
+        "mean_growth_time": "15.0 hours",
+        "frequency_growth_time": {"10": 2, "12": 3, "20": 1}
+    }
+
+    assert response.json() == expected_data
